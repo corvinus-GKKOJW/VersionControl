ee3604fff7f01ee3cac855c6054a27dcd0c081f4
Zsombor Tóth <zsombor.toth@stud.uni-corvinus.hu>
2021-10-21 09:25:06 +0200

    VaR output, helyes kod

---
+++ UserMaintenance/VaR/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VaR.entities;
+
+namespace VaR
+{
+    public partial class Form1 : Form
+    {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        public Form1()
+        {
+            InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervallum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervallum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervallum)) - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x).ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+        }
+
+        public void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+
+            //vagy lehetne így is hozzáadni:
+            /*PortfolioItem p = new PortfolioItem();
+            p.Index = "OTP";
+            p.Volume = 10;
+            Portfolio.Add(p);*/
+
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+
+        public decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim() && date <= x.TradingDay
+                            select x).First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
+    }
+}
--- UserMaintenance/VaR/Properties/AssemblyInfo.cs
+++ UserMaintenance/VaR/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("df0acc51-ee95-43d2-8805-17103012f579")]
+[assembly: Guid("5d5ff58c-ee28-4830-bf25-930b041bec49")]
 
 // Version information for an assembly consists of the following four values:
 //
