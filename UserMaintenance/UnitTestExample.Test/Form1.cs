eb6bab385ca59aa4ffdf09c055a53e728c8c27d7
Zsombor Tóth <zsombor.toth@stud.uni-corvinus.hu>
2021-11-25 19:59:54 +0100

    HappyPath

--- UserMaintenance/UnitTestExample.Test/Form1.cs
+++ UserMaintenance/UnitTestExample.Test/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using UnitTestExample.Controllers;
@@ -38,6 +39,43 @@ namespace UnitTestExample.Test
 
                 //Assert
                 Assert.AreEqual(expectedResult, actualResult);
+            }
+
+            [
+                Test,
+                TestCase("gkkojw", false),
+                TestCase("GKKOJW", false),
+                TestCase("gkkojw", false),
+                TestCase("rovid7", false),
+                TestCase("Megfelelo7", true)
+            ]
+
+            public bool ValidatePassword (string pw)
+            {
+                var lowerCase = new Regex(@"[a-z]+");
+                var upperCase = new Regex(@"[A-Z]+");
+                var number = new Regex(@"[0-9]+");
+                var nyolcHosszu = new Regex(@".{8,}");
+                return lowerCase.IsMatch(pw) && upperCase.IsMatch(pw) && number.IsMatch(pw) && nyolcHosszu.IsMatch(pw);
+            }
+
+            [
+                Test,
+                TestCase("gkkojw@uni-corvinus.hu", "Megfelelo7"),
+                TestCase("gkkojw@uni-corvinus.hu", "Megfelelo45678"),
+             ]
+            public void TestRegisterHappyPath(string email, string password)
+            {
+                //Arrange
+                var accountController = new AccountController();
+
+                //Act
+                var actualResult = accountController.Register(email, password);
+
+                //Assert
+                Assert.AreEqual(email, actualResult.Email);
+                Assert.AreEqual(password, actualResult.Password);
+                Assert.AreNotEqual(Guid.Empty, actualResult.ID);
             }
         }
     }
