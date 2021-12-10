587e4bfadeed45ae7d443f963be74100aeb40cad
Zsombor Tóth <zsombor.toth@stud.uni-corvinus.hu>
2021-12-10 17:24:05 +0100

    jatekter

--- UserMaintenance/WorldsHardestGame/Form1.cs
+++ UserMaintenance/WorldsHardestGame/Form1.cs
@@ -12,9 +12,18 @@ namespace WorldsHardestGame
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga = new GameArea();
+
         public Form1()
         {
             InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
